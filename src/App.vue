bfafc5b21c0648e3240b699476dfab1f56775f1f
sd990307@gmail.com <songdoyoon@songdoyun-ui-MacBook-Pro.local>
2022-11-23 16:26:32 +0900

    Component Fixed

--- src/App.vue
+++ src/App.vue
@@ -9,18 +9,20 @@
     </md-card>
   </div> -->
   <div id="app">
-    <DemoDrink></DemoDrink>
+    <DemoDrink1 msg1="데자와" msg2="솔의 눈" msg3="베지밀" msg4="두유 검은콩"></DemoDrink1>
+    <DemoDrink2 msg1="레드불" msg2="몬스터 에너지" msg3="몬스터 울트라" msg4="핫식스"></DemoDrink2>
   </div>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
-import DemoDrink from './components/DemoDrink.vue'
+import DemoDrink1 from './components/DemoDrink1.vue'
+import DemoDrink2 from './components/DemoDrink2.vue'
 
 export default {
   name: 'App',
   components : {
-    DemoDrink
+    DemoDrink1,
+    DemoDrink2
   }
 }
 </script>
---
+++ src/components/DemoDrink1.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="app">
+    <md-card class="md-with-hover">
+      <md-card-header>
+        <div class="md-title">
+          {{ msg1 }}
+        </div>
+        <img src="https://cdn.imweb.me/thumbnail/20201201/2a7eb5b04812d.jpg">
+      </md-card-header>
+      <md-card-content>
+        <md-button class="md-icon-button md-raised md-accent">
+          <md-icon>thumb_up</md-icon>
+        </md-button>
+      </md-card-content>
+    </md-card>
+    <md-card class="md-with-hover">
+      <md-card-header>
+        <div class="md-title">
+          {{ msg2 }}
+        </div>
+        <img src="https://img.danawa.com/prod_img/500000/945/970/img/1970945_1.jpg?shrink=330:330&_v=20211110135448">
+      </md-card-header>
+      <md-card-content>
+        <md-button class="md-icon-button md-raised md-accent">
+          <md-icon>thumb_up</md-icon>
+        </md-button>
+      </md-card-content>
+    </md-card>
+        <md-card class="md-with-hover">
+      <md-card-header>
+        <div class="md-title">
+          {{ msg3 }}
+        </div>
+      </md-card-header>
+      <md-card-content>
+        <md-button class="md-icon-button md-raised md-accent">
+          <md-icon>thumb_up</md-icon>
+        </md-button>
+      </md-card-content>
+    </md-card>
+        <md-card class="md-with-hover">
+      <md-card-header>
+        <div class="md-title">
+          {{ msg4 }}
+        </div>
+      </md-card-header>
+      <md-card-content>
+        <md-button class="md-icon-button md-raised md-accent">
+          <md-icon>thumb_up</md-icon>
+        </md-button>
+      </md-card-content>
+    </md-card>
+    
+  </div>
+</template>
+
+<style>
+  .md-card {
+    width: 320px;
+    margin: 4px;
+    display: inline-block;
+    vertical-align: top;
+  }
+</style>
+
+<script>
+export default {
+  name: 'DemoDrink2',
+  props: {
+    msg1: String,
+    msg2: String,
+    msg3: String,
+    msg4: String
+  }
+}
+</script>
